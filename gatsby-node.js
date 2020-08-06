c50ca6e6f9aef0be3c6f517c1713d4be455d405f
Christopher Collins <chris@ccollins.io>
2020-08-05 23:56:23 -0600

    fixed failed deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  * Fix: https://github.com/gaearon/react-hot-loader/issues/1227
  */
-const path = require("path")
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
 
 exports.onCreateWebpackConfig = ({ stage, actions }) => {
   if (stage.startsWith("develop")) {
@@ -14,35 +15,54 @@ exports.onCreateWebpackConfig = ({ stage, actions }) => {
           "react-dom": "@hot-loader/react-dom",
         },
       },
-    })
+    });
   }
-}
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === "MarkdownRemark") {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: "posts",
+    });
+
+    createNodeField({
+      node,
+      name: "slug",
+      value: `/posts${slug}`,
+    });
+  }
+};
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
   return new Promise((resolve, reject) => {
     graphql(`
       {
-        allContentfulBlogPost {
+        allMarkdownRemark {
           edges {
             node {
-              slug
+              fields {
+                slug
+              }
             }
           }
         }
       }
     `).then(result => {
-      result.data.allContentfulBlogPost.edges.forEach(({ node }) => {
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
         createPage({
-          path: node.slug,
+          path: node.fields.slug,
           component: path.resolve("./src/posts/PostPage.js"),
           context: {
-            slug: node.slug,
+            slug: node.fields.slug,
           },
-        })
-      })
+        });
+      });
 
-      resolve()
-    })
-  })
-}
+      resolve();
+    });
+  });
+};
